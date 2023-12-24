40d60296838fb24445c81793bf7f7f2ea0d5075b
BlazEffect <kamalinigor@gmail.com>
2023-12-24 03:36:07 +0300

    Added deletion of events

--- src/pages/Admin/Users/Events.jsx
+++ src/pages/Admin/Users/Events.jsx
@@ -1,64 +1,73 @@
 import {Space, Table} from "antd";
-import {useEffect, useState} from "react";
-import {getAllEvents} from "@/services/EventService";
+import {useContext, useEffect, useState} from "react";
 import moment from "moment";
+import * as eventService from "@/services/EventService"
+import {AuthContext} from "@/contexts/AuthContext.jsx";
 
 moment.tz.setDefault('Europe/Moscow');
 moment.locale('ru');
-
-const columns = [
-  {
-    title: 'Название мероприятия',
-    dataIndex: 'name',
-    key: 'title',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Описание',
-    dataIndex: 'description',
-    key: 'description',
-  },
-  {
-    title: 'Время начала',
-    dataIndex: 'time_start',
-    key: 'time_start',
-    render: (text) => moment.unix(text / 1000).format("LLLL")
-  },
-  {
-    title: 'Время конца',
-    dataIndex: 'time_end',
-    key: 'time_end',
-    render: (text) => moment.unix(text / 1000).format("LLLL")
-  },
-  {
-    title: 'Пользователь',
-    dataIndex: 'user_id',
-    key: 'user_id',
-  },
-  {
-    title: 'Действия',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
 
 const Events = () => {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const fetchData = async () => {
-    setLoading(true);
-    setEvents(await getAllEvents());
-    setLoading(false);
-  }
+  const { authData } = useContext(AuthContext);
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    setEvents(await eventService.getAllEvents());
+    setLoading(false);
+  }
+
+  const deleteEvent = async (cardId, token) => {
+    setLoading(true);
+    await eventService.deleteEvent(cardId, token);
+    setLoading(false);
+  }
+
+  const columns = [
+    {
+      title: 'Название мероприятия',
+      dataIndex: 'name',
+      key: 'title',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Описание',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Время начала',
+      dataIndex: 'time_start',
+      key: 'time_start',
+      render: (text) => moment.unix(text / 1000).format("LLLL")
+    },
+    {
+      title: 'Время конца',
+      dataIndex: 'time_end',
+      key: 'time_end',
+      render: (text) => moment.unix(text / 1000).format("LLLL")
+    },
+    {
+      title: 'Пользователь',
+      dataIndex: 'user_id',
+      key: 'user_id',
+    },
+    {
+      title: 'Действия',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <a onClick={() => deleteEvent(record.key, authData.token)}>Удалить событие</a>
+        </Space>
+      ),
+    },
+  ];
 
   return (
     <div>
--- src/services/EventService.jsx
+++ src/services/EventService.jsx
@@ -8,4 +8,12 @@ const getAllEvents = async () => {
   }
 }
 
-export {getAllEvents};
+const deleteEvent = async (cardId, token) => {
+  try {
+    await axios.delete('/api/delete_card?card_id=' + cardId + '&token=' + token);
+  } catch (error) {
+    throw new Error('Ошибка при выполнении запроса');
+  }
+}
+
+export {getAllEvents, deleteEvent};
